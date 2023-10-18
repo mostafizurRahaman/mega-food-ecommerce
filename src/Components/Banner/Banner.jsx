f0e96ab4f10b41ee6d88ce91ce837e965b011fda
mostafizurRahaman <fahim654326@gmail.com>
2023-10-18 15:46:45 +0600

    implement banner functionlities

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 import { useNavigate } from "react-router-dom";
 import ActionButton from "../Buttons/ActionButton";
+import { useQuery } from "@tanstack/react-query";
+import { baseURL } from "../../Configs/libs";
 
 const Banner = ({ banner }) => {
    const navigate = useNavigate();
 
-   const { title, offerPath, ButtonText, imageURL, description } = banner;
+   const { title, offerId, offerType, buttonText, imageURL, description } =
+      banner;
 
    const styles = {
       background: `linear-gradient(90deg, rgba(48, 97, 221, 0),rgba(48, 97, 221, 0)), url(${imageURL}) no-repeat`,
@@ -14,9 +17,33 @@ const Banner = ({ banner }) => {
       objectFit: "cover",
       // width: "100%"
    };
+   const { data: offerItem = {} } = useQuery({
+      queryKey: ["offerItem", offerId, offerType],
+      queryFn: async () => {
+         if (offerId && offerId) {
+            const res = await fetch(`${baseURL}/${offerType}/${offerId}`);
+            const data = await res.json();
+            console.log("data fetched", data);
+            return data.data;
+         }
+         return {};
+      },
+   });
 
    const handleAction = () => {
-      navigate(offerPath);
+      if (offerType === "category") {
+         return navigate(`/category/${offerItem?._id}`);
+      }
+      if (offerType === "sub-category") {
+         return navigate(
+            `/category/${offerItem.category.id}/${offerItem?._id}`
+         );
+      }
+      if (offerType === "product") {
+         return navigate(
+            `/category/${offerItem.category.id}/${offerItem.subCategory.id}/${offerItem?._id}`
+         );
+      }
    };
 
    return (
@@ -24,15 +51,17 @@ const Banner = ({ banner }) => {
          className="  p-10   flex items-center justify-start   "
          style={styles}
       >
-         <div className=" lg:w-2/5 flex gap-3 flex-col p-3 bg-opacity-75 rounded-md bg-white  md:bg-transparent">
+         <div className=" lg:w-2/5 flex gap-3 flex-col p-3 bg-opacity-75 rounded-md bg-white  ">
             <h2 className="text-4xl font-medium capitalize mb-3 ">{title}</h2>
             <p className="text-xl  font-normal ">{description}</p>
-            <ActionButton
-               handleAction={handleAction}
-               containerStyles="bg-primary text-lg text-secondary font-semibold rounded-lg   inline-block w-auto mr-auto mt-5 "
-            >
-               {ButtonText}
-            </ActionButton>
+            {offerItem.name && (
+               <ActionButton
+                  handleAction={handleAction}
+                  containerStyles="bg-primary text-lg text-secondary font-semibold rounded-lg   inline-block w-auto mr-auto mt-5 capitalize"
+               >
+                  {buttonText}
+               </ActionButton>
+            )}
          </div>
       </div>
    );
