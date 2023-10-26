509e03a0afecaed1838c77dfac09829f1bae0d38
mostafizurRahaman <fahim654326@gmail.com>
2023-10-26 10:51:45 +0600

    create private route

---
+++ src/Routes/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../../Components/Loading";
+import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import { useContext } from "react";
+const PrivateRoute = ({ children }) => {
+   const { user, loading } = useContext(AuthContext);
+   const location = useLocation();
+
+   if (loading) {
+      return <Loading></Loading>;
+   }
+
+   if (user?.email) {
+      return children;
+   }
+
+   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
